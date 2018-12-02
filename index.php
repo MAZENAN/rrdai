f493cea5c722b82c6501e621a3b72b6d371d4664
王宇帆 <yufanBangBang.163.com>
2018-12-02 23:28:25 +0800

    修改了

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
 <?php
-$arr = ['p1' => ['name' => '张小姐', 'age' => 16], 'p2' => ['name' => '张小姐', 'age' => 16], 'p3' => ['name' => '张小姐', 'age' => 16]];
+$arr = [
+	'p1' => ['name' => '张小姐', 'age' => 16],
+	'p2' => ['name' => '张小姐', 'age' => 16],
+	'p3' => ['name' => '张小姐', 'age' => 16],
+];
+$arr2 = [
+	'p1' => ['name' => '张小姐', 'age' => 16],
+	'p2' => ['name' => '张小姐', 'age' => 16],
+	'p3' => ['name' => '张小姐', 'age' => 16],
+];
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<style type="text/css">
+		table{
+			float: left;
+			border: 1px solid black;
+		}
+	</style>
 </head>
 <body>
+	<div>
 	<table>
 		<tr>
 			<td>姓名</td>
@@ -19,5 +35,17 @@ $arr = ['p1' => ['name' => '张小姐', 'age' => 16], 'p2' => ['name' => '张小
 				</tr>
 			<?php endforeach?>
 	</table>
+	<table>
+		<tr>
+			<td>姓名</td>
+			<td>性别</td>
+			<?php foreach ($arr2 as $key => $value): ?>
+				<tr>
+					<td><?php echo $value['name']; ?></td>
+					<td><?php echo $value['age']; ?></td>
+				</tr>
+			<?php endforeach?>
+	</table>
+	</div>
 </body>
 </html>
